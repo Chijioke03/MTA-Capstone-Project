03892f646a200ef2014541c215ac9344f1f349d5
Ceejay_codes <101662458+Chijioke03@users.noreply.github.com>
2026-02-19 14:40:34 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const registerBtn = document.querySelector(".reg")
 const menuBtn = document.getElementById("menu-btn")
 const navList = document.querySelector(".header-nav-list")
-
-
+const coursesBtn = document.querySelector(".explore")
+const coursesSection = document.querySelector(".courses")
 menuBtn.addEventListener("click", ()=> {
     navList.classList.add("show-menu")
 })
 
 registerBtn.addEventListener("click", ()=> {
     window.location.href = "register.html"
+})
+
+
+coursesBtn.addEventListener("click", ()=> {
+    coursesSection.classList.toggle("show-courses")
 })
 
